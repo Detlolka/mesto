69bbeb2edf2225a3000ea0e696e41307437c5310
Detlolka <grinchukalexey@yandex.ru>
2020-05-13 23:39:01 +0300

    Исправлены замечания

--- js/script.js
+++ js/script.js
@@ -2,30 +2,29 @@ const editButton = document.querySelector('.profile__editButton'); // Кнопк
 const popup = document.querySelector('.popup'); // Форма Popup
 const popupClose = popup.querySelector('.popup__close'); // Кнопка закрытия Popupa
 const formElement = popup.querySelector('.popup__form'); // Форма
-const nameInput = formElement.querySelector('.popup__input-name'); // Поле ввода имени    
-const jobInput = formElement.querySelector('.popup__input-about'); // Поле с доп.информацией
+const nameInput = formElement.querySelector('.popup__input_name'); // Поле ввода имени    
+const jobInput = formElement.querySelector('.popup__input_about'); // Поле с доп.информацией
 const profileName = document.querySelector('.profile__title'); // Заголовок профиля
 const profileAbout = document.querySelector('.profile__subtitle'); // Подзаголовок профиля
 
-editButton.addEventListener('click', function () {     //  Вызов формы
+function openForm () {                              //  Вызов формы
     popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
-    jobInput.value = profileAbout.textContent;    
+    jobInput.value = profileAbout.textContent;
+}
 
-});
-
-popupClose.addEventListener("click", function() {    //  Закрытие формы    
-    
+function closeForm() {                              //  Закрытие формы    
     popup.classList.remove('popup_opened');
-});
+}
 
 function formSubmitHandler (evt) {                  // Отправка данных в профиль
     evt.preventDefault();       
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
-    
+    closeForm();    
 }
 
+editButton.addEventListener('click', openForm);
+popupClose.addEventListener("click", closeForm); 
 formElement.addEventListener('submit', formSubmitHandler);
 
